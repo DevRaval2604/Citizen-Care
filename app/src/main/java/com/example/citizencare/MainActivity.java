949230b6e1ee639af91eb6ec7246051b518f6e67
DevRaval2604 <devraval2004@gmail.com>
2024-01-09 23:32:44 +0530

    Final Year Project

--- app/src/main/java/com/example/citizencare/MainActivity.java
+++ app/src/main/java/com/example/citizencare/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.citizencare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] item={"Fogger Truck","CCTV Camera","Garbage Truck","Dustbin"};
+        String[] item2={"Road Issue","Garbage Pile-Ups","Streetlight Issue","Drainage System Blockage","Cattle Menace","Park Issue","Mosquito Infestation","Fallen Tree"};
+        AutoCompleteTextView autoCompleteTextView;
+        AutoCompleteTextView autoCompleteTextView1;
+        ArrayAdapter<String> adapterItems;
+        ArrayAdapter<String> adapterItems1;
+        autoCompleteTextView=findViewById(R.id.autoComplete1);
+        autoCompleteTextView1=findViewById(R.id.autoComplete2);
+        adapterItems= new ArrayAdapter<>(this, R.layout.list_item, item);
+        adapterItems1= new ArrayAdapter<>(this, R.layout.list_item, item2);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener((adapterView, view, i, l) -> {
+            String Item=adapterView.getItemAtPosition(i).toString();
+            Toast.makeText(MainActivity.this,"Item:"+Item,Toast.LENGTH_SHORT).show();
+            Intent inext;
+            inext=new Intent(MainActivity.this, Login.class);
+            startActivity(inext);
+        });
+        autoCompleteTextView1.setAdapter(adapterItems1);
+        autoCompleteTextView1.setOnItemClickListener((adapterView, view, i, l) -> {
+            String Item=adapterView.getItemAtPosition(i).toString();
+            Toast.makeText(MainActivity.this,"Item:"+Item,Toast.LENGTH_SHORT).show();
+            Intent inext;
+            inext=new Intent(MainActivity.this, Login.class);
+            startActivity(inext);
+        });
     }
 }
