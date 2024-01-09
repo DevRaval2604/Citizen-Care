2f11e2bccec732382454514546485e9ee5567848
DevRaval2604 <devraval2004@gmail.com>
2024-01-10 01:15:04 +0530

    manage service type and manage complaint type design

---
+++ app/src/main/java/com/example/citizencare/ManageComplaintType.java
@@ -0,0 +1,14 @@
+package com.example.citizencare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageComplaintType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_complaint_type);
+    }
+}
