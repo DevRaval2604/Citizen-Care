982c54b53f5fdeb0af5892e748709ef045beec60
DevRaval2604 <devraval2004@gmail.com>
2024-01-10 23:16:02 +0530

    Final Year Project

--- app/src/main/java/com/example/citizencare/MainActivity.java
+++ app/src/main/java/com/example/citizencare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.citizencare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,37 +9,67 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] item={"Fogger Truck","CCTV Camera","Garbage Truck","Dustbin"};
-        String[] item2={"Road Issue","Garbage Pile-Ups","Streetlight Issue","Drainage System Blockage","Cattle Menace","Park Issue","Mosquito Infestation","Fallen Tree"};
-        AutoCompleteTextView autoCompleteTextView;
-        AutoCompleteTextView autoCompleteTextView1;
-        ArrayAdapter<String> adapterItems;
-        ArrayAdapter<String> adapterItems1;
-        autoCompleteTextView=findViewById(R.id.autoComplete1);
-        autoCompleteTextView1=findViewById(R.id.autoComplete2);
-        adapterItems= new ArrayAdapter<>(this, R.layout.list_item, item);
-        adapterItems1= new ArrayAdapter<>(this, R.layout.list_item, item2);
-        autoCompleteTextView.setAdapter(adapterItems);
-        autoCompleteTextView.setOnItemClickListener((adapterView, view, i, l) -> {
-            String Item=adapterView.getItemAtPosition(i).toString();
-            Toast.makeText(MainActivity.this,"Item:"+Item,Toast.LENGTH_SHORT).show();
-            Intent inext;
-            inext=new Intent(MainActivity.this, Login.class);
-            startActivity(inext);
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        DatabaseReference myRef=database.getReference("Service Type");
+        DatabaseReference myRef1=database.getReference("Complaint Type");
+        AutoCompleteTextView autoCompleteTextView=findViewById(R.id.autoComplete1);
+        AutoCompleteTextView autoCompleteTextView1=findViewById(R.id.autoComplete2);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                List<String> suggestions=new ArrayList<>();
+                for (DataSnapshot snapshot:datasnapshot.getChildren()){
+                    String value=snapshot.getValue(String.class);
+                    suggestions.add(value);
+                }
+                ArrayAdapter<String> adapter=new ArrayAdapter<>(MainActivity.this,R.layout.list_item,suggestions);
+                autoCompleteTextView.setAdapter(adapter);
+                autoCompleteTextView.setOnItemClickListener((adapterView, view, i, l) -> {
+                    Intent iNext=new Intent(MainActivity.this, Login.class);
+                    startActivity(iNext);
+                    Toast.makeText(getApplicationContext(),"Item: "+suggestions.get(i),Toast.LENGTH_SHORT).show();
+                });
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
-        autoCompleteTextView1.setAdapter(adapterItems1);
-        autoCompleteTextView1.setOnItemClickListener((adapterView, view, i, l) -> {
-            String Item=adapterView.getItemAtPosition(i).toString();
-            Toast.makeText(MainActivity.this,"Item:"+Item,Toast.LENGTH_SHORT).show();
-            Intent inext;
-            inext=new Intent(MainActivity.this, Login.class);
-            startActivity(inext);
+        myRef1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                List<String> suggestions=new ArrayList<>();
+                for (DataSnapshot snapshot:datasnapshot.getChildren()){
+                    String value=snapshot.getValue(String.class);
+                    suggestions.add(value);
+                }
+                ArrayAdapter<String> adapter=new ArrayAdapter<>(MainActivity.this,R.layout.list_item,suggestions);
+                autoCompleteTextView1.setAdapter(adapter);
+                autoCompleteTextView1.setOnItemClickListener((adapterView, view, i, l) -> {
+                    Intent iNext=new Intent(MainActivity.this, Login.class);
+                    startActivity(iNext);
+                    Toast.makeText(getApplicationContext(),"Item: "+suggestions.get(i),Toast.LENGTH_SHORT).show();
+                });
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
     }
 }
