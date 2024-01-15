14e480a829bb0ecef033f14aeb681c6302b948af
DevRaval2604 <devraval2004@gmail.com>
2024-01-15 19:07:15 +0530

    Final Year Project

--- app/src/main/java/com/example/citizencare/Feedback.java
+++ app/src/main/java/com/example/citizencare/Feedback.java
@@ -3,6 +3,9 @@ package com.example.citizencare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
 
 public class Feedback extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Feedback extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        TextView text2=findViewById(R.id.text2);
+        TextView text3=findViewById(R.id.text3);
+        String services="Services";
+        SpannableString content1=new SpannableString(services);
+        content1.setSpan(new UnderlineSpan(),0,content1.length(),0);
+        text2.setText(content1);
+        String complaints="Complaints";
+        SpannableString content2=new SpannableString(complaints);
+        content2.setSpan(new UnderlineSpan(),0,content2.length(),0);
+        text3.setText(content2);
     }
 }
