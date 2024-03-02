d1bc38da34c1687601c589d242b00325ad91dbf8
DevRaval2604 <devraval2004@gmail.com>
2024-03-02 13:38:43 +0530

    Final Year Project

--- app/src/main/java/com/example/citizencare/Reports_UserData_Admin.java
+++ app/src/main/java/com/example/citizencare/Reports_UserData_Admin.java
@@ -1,9 +1,15 @@
 package com.example.citizencare;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -19,12 +25,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Reports_UserData_Admin extends AppCompatActivity {
-
     AutoCompleteTextView autoCompleteTextView;
     List<String> suggestions=new ArrayList<>();
     ArrayAdapter<String> adapter;
     RecyclerView recyclerView;
     UserDataAdminAdapter userDataAdminAdapter;
+    private final static int REQUEST_WRITE_EXTERNAL_STORAGE=101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +40,7 @@ public class Reports_UserData_Admin extends AppCompatActivity {
 
         Button btnGenerateReports = findViewById(R.id.button_generate_reports);
         btnGenerateReports.setOnClickListener(view -> {
-            generatePDF();
+            requestStoragePermission();
             Toast.makeText(Reports_UserData_Admin.this, "PDF Generated Successfully", Toast.LENGTH_LONG).show();
         });
 
@@ -91,6 +97,25 @@ public class Reports_UserData_Admin extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         userDataAdminAdapter.stopListening();
+    }
+
+    private void requestStoragePermission(){
+       if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+           ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_WRITE_EXTERNAL_STORAGE);
+       }else {
+           generatePDF();
+       }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                generatePDF();
+            } else {
+                Toast.makeText(Reports_UserData_Admin.this, "Storage permission denied,please allow permission to access storage", Toast.LENGTH_LONG).show();
+            }
+        }
     }
     public void generatePDF(){
         PdfGenerator.getBuilder().setContext(Reports_UserData_Admin.this).fromViewSource().fromView(recyclerView).setFileName("User-Data-Report").setFolderNameOrPath("User_Data_Reports/").actionAfterPDFGeneration(PdfGenerator.ActionAfterPDFGeneration.OPEN).build(new PdfGeneratorListener() {
